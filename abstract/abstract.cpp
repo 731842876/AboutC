b5099623961c7f55bd21e5238b03e0f2c6216ac2
ATLAS <731842876@qq.com>
2021-06-28 18:38:23 +0800

    添加纯虚函数和抽象含泪

--- abstract/abstract.cpp
+++ abstract/abstract.cpp
@@ -2,10 +2,21 @@
 using namespace std;
 
 
-//@File      : abstract
-//@Author    : ATLAS
-//@Time      : 2021/6/28 17:57:40
+// @File      : abstract
+// @Author    : ATLAS
+// @brief     : 纯虚函数为没有具体实现，虚函数不一定有具体实现。
+// @Time      : 2021/6/28 17:57:40
 
+
+//包含了纯虚函数的类为抽象类，因为纯虚函数没具体实现，类也无法初始化对象。
+class test {
+
+public:
+	//通过赋值为0来声明纯虚函数
+	virtual void show() = 0;
+};
 int main() {
-	cout << "hey" << endl;
+	
+	//test a;//抽象类无法初始化对象
+	test* a1;//可初始化指针
 }
