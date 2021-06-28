fef6ea4fd119541de8597166f2e20b0ee26b1845
ATLAS <731842876@qq.com>
2021-06-28 17:58:42 +0800

    add abstract

--- abstract/abstract.cpp
+++ abstract/abstract.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 //@File      : abstract
 //@Author    : ATLAS
-//@Time      : 2021/6/28 17:45:07
+//@Time      : 2021/6/28 17:57:40
 
 int main() {
-
+	cout << "hey" << endl;
 }
